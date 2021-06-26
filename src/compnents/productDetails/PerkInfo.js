358846638f14c8bb05b4f138a35fb59052f35d91
Iris Huang <lyaui@outloook.com>
2021-06-27 02:46:59 +0800

    贊助方案卡片基礎樣式製作

--- src/compnents/productDetails/PerkInfo.js
+++ src/compnents/productDetails/PerkInfo.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PerkItemCard from '../../compnents/productDetails/PerkItemCard';
+import PerkImage from '../../assets/images/plan_img.jpeg';
 
 function PerkInfo() {
-  return <div>perk</div>;
+  const PerkItems = {
+    image: PerkImage,
+    isLimitedTimeOffer: true,
+    leftDays: '2 days 20:05:20',
+    title: '買一送一，只有兩百組別錯過',
+    price: 1600,
+    sponsoredTimes: 88,
+    limitedAmount: 200,
+    desc: '受就相法大夠局來畫師我只這然以放灣得的後人心年我物我年，或人有動與次出女親構算帶年行著所間女。',
+  };
+  const renderPerkItems = [...Array(2)].map((item, idx) => (
+    <PerkItemCard className='mb-4' PerkItems={PerkItems} key={idx} />
+  ));
+  return <div className='col-span-4'>{renderPerkItems}</div>;
 }
 
 export default PerkInfo;
---
+++ src/compnents/productDetails/PerkItemCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Badge from '../UI/Badge';
+
+function PerkItemCard({ PerkItems }) {
+  const { image, isLimitedTimeOffer, leftDays, title, price, sponsoredTimes, limitedAmount, desc } =
+    PerkItems;
+  return (
+    <div className='border rounded-lg border-gray-cardBorder overflow-hidden mb-4'>
+      <img src={image} alt={title} className='h-28 w-full object-cover' />
+      <div className='px-4 py-2'>
+        <div className='flex justify-between pb-2 border-b text-font-link tracking-wider'>
+          {isLimitedTimeOffer && <span>限時優惠</span>}
+          <span className='ml-auto'>{leftDays}</span>
+        </div>
+        <div className='text-lg font-bold tracking-wider text-font-title py-2'>{title}</div>
+        <div className='flex mb-2'>
+          <Badge className='bg-gray-tabBorder text-gray mr-2'>
+            已被贊助 <span>{sponsoredTimes}</span> 次
+          </Badge>
+          <Badge>
+            限量 <span class='text-yellow'>{limitedAmount}</span> 個
+          </Badge>
+        </div>
+        <div className='text-gray tracking-wider mb-4'> {desc}</div>
+      </div>
+    </div>
+  );
+}
+
+export default PerkItemCard;
