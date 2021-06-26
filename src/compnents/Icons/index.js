4f738535d80d7495650cdb5e1463cdba1ede32f7
Iris Huang <lyaui@outloook.com>
2021-06-26 22:17:29 +0800

    建立 Icon 管理檔案

---
+++ src/compnents/Icons/index.js
@@ -0,0 +1,6 @@
+import Badge from '../../assets/icons/icon_good01.svg';
+import Like from '../../assets/icons/icon_good02.svg';
+import Blob from '../../assets/icons/icon_good03.svg';
+import Crown from '../../assets/icons/icon_good04.svg';
+
+export { Badge, Like, Blob, Crown };
