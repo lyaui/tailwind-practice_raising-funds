839d5e03499b9f30ea8eecedee0b678e2fc8cd55
Iris Huang <lyaui@outloook.com>
2021-06-27 02:47:37 +0800

    badge 樣式調整

--- src/compnents/UI/Badge.js
+++ src/compnents/UI/Badge.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-function Badge({ children }) {
-  // dark / light @apply
-  //   const darkClasses = '';
-  //   const lightClasses = '';
-  return (
-    <div className='w-max tracking-wide px-3 rounded text-bold bg-gray text-white'>{children}</div>
-  );
+function Badge({ className, children }) {
+  const classes = className ? className : 'bg-gray text-white';
+  return <div className={`w-max tracking-wide px-3 rounded ${classes}`}>{children}</div>;
 }
 
 export default Badge;
